ef5ca89519397ef5215775d4c1473a7de0c5eece
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-04 23:44:57 +0200

    dodanie paginacji i walidacji

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -2,7 +2,7 @@ class FortunesController < ApplicationController
   # GET /fortunes
   # GET /fortunes.json
   def index
-    @fortunes = Fortune.all
+    @fortunes = Fortune.order('created_at DESC').page(params[:page]).per_page(10)
 
     respond_to do |format|
       format.html # index.html.erb
