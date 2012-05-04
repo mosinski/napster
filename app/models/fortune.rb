690fa685f4a32020e935abc7190f8e66be1506bb
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-05 00:16:06 +0200

    zwiekszenie ilości znaków dla tekst

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -1,5 +1,5 @@
 class Fortune < ActiveRecord::Base
-  validates :tekst, length: { maximum: 1000 }
+  validates :tekst, length: { maximum: 5000 }
   validates :wykonawca, length: { in: 2..20 }, allow_blank: true
   validates :tytul, length: { in: 2..20 }, allow_blank: true
   validates :album, length: { maximum: 20 }
