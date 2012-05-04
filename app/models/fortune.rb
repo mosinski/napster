a7fa1295d63fba9c8e56ad1e720faa1583bef130
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-04 23:37:10 +0200

    dodanie paginacji i walidacji

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -1,2 +1,6 @@
 class Fortune < ActiveRecord::Base
+  validates :tekst, length: { maximum: 1000 }
+  validates :wykonawca, length: { in: 2..20 }, allow_blank: true
+  validates :tytul, length: { in: 2..20 }, allow_blank: true
+  validates :album, length: { maximum: 20 }
 end
