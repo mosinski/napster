bccf896bce4249a2736efedc9dc0f8ef95acafdb
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-06 20:03:47 +0200

    zwiekszenie dlugości tytułu

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -1,6 +1,6 @@
 class Fortune < ActiveRecord::Base
   validates :tekst, length: { maximum: 5000 }
-  validates :wykonawca, length: { in: 2..20 }, allow_blank: true
-  validates :tytul, length: { in: 2..20 }, allow_blank: true
+  validates :wykonawca, length: { in: 2..30 }, allow_blank: true
+  validates :tytul, length: { in: 2..40 }, allow_blank: true
   validates :album, length: { maximum: 20 }
 end
