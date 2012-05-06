c14ee8eb9b86e670d8c3dcde08ae0b43ae27278d
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-06 20:07:05 +0200

    zwiekszenie dlugosci albumu

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -2,5 +2,5 @@ class Fortune < ActiveRecord::Base
   validates :tekst, length: { maximum: 5000 }
   validates :wykonawca, length: { in: 2..30 }, allow_blank: true
   validates :tytul, length: { in: 2..40 }, allow_blank: true
-  validates :album, length: { maximum: 20 }
+  validates :album, length: { maximum: 35 }
 end
