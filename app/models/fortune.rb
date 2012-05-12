ea2008b74295d1c591c8514383abde8c9a3e7213
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-12 16:32:16 +0200

    wyszukiwanie tylko Wykonawce

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -2,7 +2,6 @@ class Fortune < ActiveRecord::Base
 def self.search(search, page)
   paginate :per_page => 10, :page => page,
            :conditions => ['wykonawca like ?', "%#{search}%"], :order => 'wykonawca'
-	   :conditions => ['tytul like ?', "%#{search}%"], :order => 'tytul'
 end
   validates :wykonawca, length: { in: 2..30 }, allow_blank: true
   validates :tytul, length: { in: 2..40 }, allow_blank: true
