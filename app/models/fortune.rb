08c94ad8d39963abf109abc9a5ff19ca47ef7a8b
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-12 12:30:06 +0200

    poprawka

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -1,7 +1,9 @@
 class Fortune < ActiveRecord::Base
 def self.search(search, page)
   paginate :per_page => 10, :page => page,
-           :conditions => ['wykonawca like ?', "%#{search}%"], :order => 'wykonawca'
+           :conditions => ['wykonawca like ?', "%#{search}%"], :order => 'wykonawca',
+	   :conditions => ['tytul like ?', "%#{search}%"], :order => 'tytul',
+	   :conditions => ['album like ?', "%#{search}%"], :order => 'album'
 end
   validates :tekst, length: { maximum: 5000 }
   validates :wykonawca, length: { in: 2..30 }, allow_blank: true
