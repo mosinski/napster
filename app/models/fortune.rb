81a5ec1c9d60e52d768f211994aa266b5c0d2b44
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-12 14:24:50 +0200

    poprawa wyszukiwarki

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -2,9 +2,10 @@ class Fortune < ActiveRecord::Base
 def self.search(search, page)
   paginate :per_page => 10, :page => page,
            :conditions => ['wykonawca like ?', "%#{search}%"], :order => 'wykonawca',
+	   :conditions => ['tytul like ?', "%#{search}%"], :order => 'tytul'
 end
-  validates :tekst, length: { maximum: 5000 }
   validates :wykonawca, length: { in: 2..30 }, allow_blank: true
   validates :tytul, length: { in: 2..40 }, allow_blank: true
   validates :album, length: { maximum: 35 }
+  validates :tekst, length: { maximum: 5000 }
 end
