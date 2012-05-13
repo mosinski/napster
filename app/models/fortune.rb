d27963f75525d7f59f5a2e39a56a5351b63e011e
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-13 14:25:50 +0200

    poprawa validacji

--- app/models/fortune.rb
+++ app/models/fortune.rb
@@ -3,8 +3,8 @@ def self.search(search, page)
   paginate :per_page => 10, :page => page,
            :conditions => ['wykonawca like :q or tytul like :q or album like :q', q: "%#{search}%"], :order => 'wykonawca'
 end
-  validates :wykonawca, length: { in: 2..30 }, allow_blank: true
-  validates :tytul, length: { in: 2..40 }, allow_blank: true
-  validates :album, length: { maximum: 35 }
-  validates :tekst, length: { maximum: 5000 }
+  validates :wykonawca, length: { in: 2..30 }, allow_blank: false
+  validates :tytul, length: { in: 2..40 }, allow_blank: false
+  validates :album, length: { maximum: 35 }, allow_blank: true
+  validates :tekst, length: { maximum: 5000 }, allow_blank: true
 end
