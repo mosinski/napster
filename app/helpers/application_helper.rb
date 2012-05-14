626427bf1c6cfe35b91f51c7b1ed4ec58dc8f878
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-14 17:34:01 +0200

    dodanie małego avatara w navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ def avatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase  
     "http://gravatar.com/avatar/#{gravatar_id}.png?d=http%3A%2F%2Fasciicasts.com%2Fimages%2Frails.png"  
   end
+def avatar_url_small(user)  
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase  
+    "http://gravatar.com/avatar/#{gravatar_id}.png?d=http%3A%2F%2Fasciicasts.com%2Fimages%2Frails.png?s=50"  
+  end
 end
