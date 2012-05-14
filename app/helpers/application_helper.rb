02f5fdd2b9b350a26d528ec25030bf2e624554a1
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-14 23:41:57 +0200

    poprawa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,11 @@ def sortable(column, title = nil)
 end
 def avatar_url(user)  
     if user.avatar_url.present?
-      user.avatar_url
+       user.avatar_url
     else
       default_url = "http://icons.iconarchive.com/icons/gakuseisean/ivista-2/256/Misc-User-icon.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{(default_url)}"
     end    
-end
+  end
 end
