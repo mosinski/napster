625d3879de3e6f9b7d173140d2ba78a6b4fbdf74
Miłosz Osiński <mylosz@yahoo.pl>
2012-05-14 17:18:33 +0200

    dodanie avatara z gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ def sortable(column, title = nil)
   direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
   link_to title, {:sort => column, :direction => direction}, {:class => css_class}
 end
+def avatar_url(user)  
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase  
+    "http://gravatar.com/avatar/#{gravatar_id}.png"  
+  end
 end
