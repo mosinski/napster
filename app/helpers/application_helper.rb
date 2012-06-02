01ca05c7fddd319242c476d1f67cbbe18ba1afcd
Miłosz Osiński <mylosz@yahoo.pl>
2012-06-02 12:48:10 +0200

    dodanie tytułów stron

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,7 @@ def avatar_url(user)
      gravatar_id = Digest::MD5::hexdigest(user.email).downcase  
     "http://gravatar.com/avatar/#{gravatar_id}.png?d=http://icons.iconarchive.com/icons/gakuseisean/ivista-2/256/Misc-User-icon.png"
 end
+def title(page_title)
+  content_for(:title) { page_title }
+end
 end
