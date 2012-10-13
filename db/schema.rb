457ae692c3eda70695933ac68f7fda1ce846360d
Miłosz Osiński <mylosz@yahoo.pl>
2012-10-13 21:04:06 +0200

    add remember me to first page

--- db/schema.rb
+++ db/schema.rb
@@ -54,13 +54,9 @@ ActiveRecord::Schema.define(:version => 20120728103953) do
     t.string   "crypted_password"
     t.string   "password_salt"
     t.string   "persistence_token"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-    t.integer  "checkboxgot",         :default => 0
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
+    t.integer  "checkboxgot",       :default => 0
     t.string   "avatarurl"
   end
 
