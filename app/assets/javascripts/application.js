2b432342cfdb7fccf5025874c56b2b2b515df62c
Miłosz Osiński <mylosz@yahoo.pl>
2012-10-31 23:31:20 +0100

    dodanie iOS Checkboxes do edycji konta

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require ios-checkboxes
 //= require_tree .
 // facebook recommend button                                                    
 $(function() {
