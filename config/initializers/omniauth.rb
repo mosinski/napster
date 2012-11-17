b1caeb87db3fd38e29602e8e5776818c01fd9f41
Miłosz Osiński <mylosz@yahoo.pl>
2012-11-17 21:20:31 +0100

    proba schowanie api key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
-use OmniAuth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['facebook_KEY'], ENV['facebook_SECRET']
   provider :github, ENV['github_KEY'], ENV['github_SECRET']
 end
-
