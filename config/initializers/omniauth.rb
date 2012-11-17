cbe7bfe450d084151dfa00b6010067281c8bc043
Miłosz Osiński <mylosz@yahoo.pl>
2012-11-17 20:32:49 +0100

    Ukrycie app_key i secret_key

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '292604074183737', '9ac0d01c2a4ba743030d88884596d82a'
-  provider :github, '553c7f75c562234c1a87', '20ead91c4728b206c48a72b227787ab13ffd187b'
-end
-
