d0b206c149571056f45750cb0f15875b3293bb7e
Miłosz Osiński <mylosz@yahoo.pl>
2012-11-17 22:22:29 +0100

    popraweczka

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middlew are.use OmniAuth::Builder do
+use OmniAuth::Builder do
  provider :facebook, AppConfig.facebook.key, AppConfig.facebook.secret
  provider :github, AppConfig.github.key, AppConfig.github.secret
 end
