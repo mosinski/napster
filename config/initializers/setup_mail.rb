35ecb1857e92413b3b713c23a952bac6c6488e3f
Miłosz Osiński <mylosz@yahoo.pl>
2013-01-08 22:00:32 +0100

    heroku config vars

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",  
   :port                 => 587,
   :user_name            => "mosinski.blog.newsletter@gmail.com",  
-  :password             => "Z1mnac0la",  
+  :password             => ENV['GMAIL_PASSWORD'],  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }
